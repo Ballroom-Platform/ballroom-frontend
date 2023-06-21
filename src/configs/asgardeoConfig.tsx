550d1596edf08fb3cfbddfa3c377641200942e6e
ManujaDewmina <manuja1bc@gmail.com>
2023-06-21 10:54:58 +0530

    Asgardeo ballroom setup

--- src/configs/asgardeoConfig.tsx
+++ src/configs/asgardeoConfig.tsx
@@ -1,7 +1,7 @@
 export const asgardeoConfig = {
     signInRedirectURL: "https://localhost:3000/loginHandler",
     signOutRedirectURL: "https://localhost:3000/",
-    clientID: "aogGrdkYtx9AS2wUoPhtlbQBq48a",
-    baseUrl: "https://api.asgardeo.io/t/ballroom",
+    clientID: "HmHRVc8MjpPrTadr4185oKLPinAa",
+    baseUrl: "https://api.asgardeo.io/t/ballroomhackathon",
     scope: [ "openid","profile" ]
 }
