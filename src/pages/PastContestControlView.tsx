7de7c566fcaa015c781d663a31e31d191d2f08c1
manuja dewmina <manuja1bc@gmail.com>
2023-08-29 14:12:36 +0530

    Bug fix getting registrants

--- src/pages/PastContestControlView.tsx
+++ src/pages/PastContestControlView.tsx
@@ -98,7 +98,7 @@ const PastContestControlView = () => {
         getReadmeContest(axiosPrivate, contestId!, getReadmeSucess, getReadmeFail);
         getReport(axiosPrivate, contestId!, (res: any) => setReport(res.data), () => console.log("Error occured"));
         csv = convertToCSV(report);
-        getContestantRegistrants(axiosIns,contestId!, (res: any) => {setregistrants((prevstate: any) =>prevstate ? [...prevstate, ...res.data] : [{}]);},() => console.log("ERROR OCCURRED."));
+        getContestantRegistrants(axiosIns,contestId!, (res: any) => {setregistrants((prevstate: any) =>prevstate ? [...res.data] : [{}]);},() => console.log("ERROR OCCURRED."));
     },[]);
     
     return ( 
