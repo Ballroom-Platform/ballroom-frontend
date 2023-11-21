cb29061df3ebbbce7bc3afc71d63359307e45308
Charuka Tharindu <tharinduu@wso2.com>
2023-11-21 11:45:48 +0530

    Update bff URLs

--- src/links/backend.tsx
+++ src/links/backend.tsx
@@ -1,9 +1,9 @@
-const BASE_URL = "http://localhost:9099";
+const BASE_URL = "http://bff-1921610396:9099/";
 
 export const BFF_URLS = {
-    uploadService: BASE_URL + "/uploadService",
-    contestService: BASE_URL + "/contestService",
-    challengeService: BASE_URL + "/challengeService",
-    userService: BASE_URL + "/userService",
-    submissionService: BASE_URL + "/submissionService",
+    uploadService: "http://uploadservice-1080067894:9094/uploadService",
+    contestService: "http://contestservice-2104489754:9098/contestService",
+    challengeService: "http://challengeservice-2732823004:9096/challengeService",
+    userService: "http://userservice-985653509:9095/userService",
+    submissionService: "http://scoreservice-2003261263:9092/submissionService",
 }
