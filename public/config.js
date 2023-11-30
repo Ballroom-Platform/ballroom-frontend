bb39797be81d64f2fb30deb29c08eebfc5d2f58a
Charuka Tharindu <tharinduu@wso2.com>
2023-11-30 14:30:46 +0530

    Remove config.js file

--- public/config.js
+++
@@ -1,7 +0,0 @@
-window.config = {
-    signInRedirectURL: "https://localhost:3000/loginHandler",
-    signOutRedirectURL: "https://localhost:3000/",
-    clientID: process.env.REACT_APP_CLIENT_ID!,
-    baseUrl: "https://api.asgardeo.io/t/ballroomhackathon",
-    scope: [ "openid","profile" ]
-    };
