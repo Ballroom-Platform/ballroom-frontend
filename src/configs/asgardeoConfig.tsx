caa514b70e7f098830ffb3584fd42ee1ea63c51c
Tharindu Udupitiya <66210480+udda1996@users.noreply.github.com>
2023-11-30 02:14:55 +0000

    Update asgardeoConfig.tsx with URLs

--- src/configs/asgardeoConfig.tsx
+++ src/configs/asgardeoConfig.tsx
@@ -1,6 +1,6 @@
 export const asgardeoConfig = {
-    signInRedirectURL: "https://2b34f7b5-5b06-4f55-ba18-16bffa3b1bba.e1-us-east-azure.choreoapps.dev/loginHandler",
-    signOutRedirectURL: "https://2b34f7b5-5b06-4f55-ba18-16bffa3b1bba.e1-us-east-azure.choreoapps.dev",
+    signInRedirectURL: "https://79c26f75-8047-4614-b2b2-bd42e570502f.e1-us-east-azure.choreoapps.dev/loginHandler",
+    signOutRedirectURL: "https://79c26f75-8047-4614-b2b2-bd42e570502f.e1-us-east-azure.choreoapps.dev",
     clientID: process.env.REACT_APP_CLIENT_ID!,
     baseUrl: "https://api.asgardeo.io/t/ballroomhackathon",
     scope: [ "openid","profile" ]
