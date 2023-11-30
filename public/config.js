5bf8dc31af551f5f791770d448ef1d4d6dcadc5b
Charuka Tharindu <tharinduu@wso2.com>
2023-11-30 15:11:05 +0530

    Add the config.js file

---
+++ public/config.js
@@ -0,0 +1,7 @@
+window.config = {
+    signInRedirectURL: "https://localhost:3000/loginHandler",
+    signOutRedirectURL: "https://localhost:3000/",
+    clientID: "lf_YmI5bt6jMFdL7iWT0e2kFvgga",
+    baseUrl: "https://api.asgardeo.io/t/ballroomhackathon",
+    scope: [ "openid","profile" ]
+}
