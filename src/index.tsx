c70d0a9b2a8387a9999c9b9cd757f043fc87af13
Charuka Tharindu <tharinduu@wso2.com>
2023-12-01 10:10:24 +0530

    Change Asgardeo configuration path

--- src/index.tsx
+++ src/index.tsx
@@ -4,31 +4,16 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from '@asgardeo/auth-react';
-// import { asgardeoConfig } from './configs/asgardeoConfig';
+import { asgardeoConfig } from './configs/asgardeoConfig';
 import { defaultTheme } from './themes/default';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import AppContextProvider from './contexts/AppContext';
-// interface Config {
-//     signInRedirectURL: string;
-//     signOutRedirectURL: string;
-//     clientID: string;
-//     baseUrl: string;
-//   };
-// declare global {
-//   interface Window {
-//     config: Config;
-//   }
-// }
 
-// const asgardeoConfig = {
-//   signInRedirectURL: window.config.signInRedirectURL,
-//   signOutRedirectURL: window.config.signOutRedirectURL,
-//   clientID: window.config.clientID!,
-//   baseUrl: window.config.baseUrl,
-//   scope: [ "openid","profile" ]
-// }
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+root.render(
     <ThemeProvider theme={defaultTheme}>
       <AuthProvider config={asgardeoConfig}>
         <AppContextProvider>
