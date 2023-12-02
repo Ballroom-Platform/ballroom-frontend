14b5c7d4c12d772d2874d8cf1a4579ea420af846
Charuka Tharindu <tharinduu@wso2.com>
2023-12-02 15:38:34 +0530

    Introduce .env file

--- src/configs/asgardeoConfig.tsx
+++ src/configs/asgardeoConfig.tsx
@@ -1,7 +1,7 @@
 export const asgardeoConfig = {
     signInRedirectURL: "https://2b34f7b5-5b06-4f55-ba18-16bffa3b1bba.e1-us-east-azure.choreoapps.dev/loginHandler",
     signOutRedirectURL: "https://2b34f7b5-5b06-4f55-ba18-16bffa3b1bba.e1-us-east-azure.choreoapps.dev/",
-    clientID: "lf_YmI5bt6jMFdL7iWT0e2kFvgga",
+    clientID: process.env.REACT_APP_CLIENT_ID!,
     baseUrl: "https://api.asgardeo.io/t/ballroomhackathon",
     scope: [ "openid","profile" ]
 }
