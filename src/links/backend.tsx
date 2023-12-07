fd362b994cf064d1ba094ba2d5dde4e33cd0d664
Charuka Tharindu <tharinduu@wso2.com>
2023-12-07 11:45:19 +0530

    Update service URLs

--- src/links/backend.tsx
+++ src/links/backend.tsx
@@ -1,7 +1,7 @@
 export const BFF_URLS = {
-    uploadService: "https://c9e168dc-8130-42d7-865d-40b1917b0704-dev.e1-us-east-azure.choreoapis.dev/jpac/uploadservice/uploadservice-972/v1.0/uploadService",
-    contestService: "https://c9e168dc-8130-42d7-865d-40b1917b0704-dev.e1-us-east-azure.choreoapis.dev/jpac/contestservice/contestservice-ced/v1.0/contestService",
-    challengeService: "https://c9e168dc-8130-42d7-865d-40b1917b0704-dev.e1-us-east-azure.choreoapis.dev/jpac/challengeservice/challengeservice-c0d/v1.0/challengeService",
-    userService: "https://c9e168dc-8130-42d7-865d-40b1917b0704-dev.e1-us-east-azure.choreoapis.dev/jpac/userservice/userservice-e4a/v1.0/userService",
-    submissionService: "https://c9e168dc-8130-42d7-865d-40b1917b0704-dev.e1-us-east-azure.choreoapis.dev/jpac/scoreservice/submissionservice-4ec/v1.1/submissionService",
+    uploadService: "https://c9e168dc-8130-42d7-865d-40b1917b0704-dev.e1-us-east-azure.choreoapis.dev/jpac/uploadservice/uploadservice-972/v1.0",
+    contestService: "https://c9e168dc-8130-42d7-865d-40b1917b0704-dev.e1-us-east-azure.choreoapis.dev/jpac/contestservice/contestservice-ced/v1.0",
+    challengeService: "https://c9e168dc-8130-42d7-865d-40b1917b0704-dev.e1-us-east-azure.choreoapis.dev/jpac/challengeservice/challengeservice-c0d/v1.0",
+    userService: "https://c9e168dc-8130-42d7-865d-40b1917b0704-dev.e1-us-east-azure.choreoapis.dev/jpac/userservice/userservice-e4a/v1.0",
+    submissionService: "https://c9e168dc-8130-42d7-865d-40b1917b0704-dev.e1-us-east-azure.choreoapis.dev/jpac/scoreservice/submissionservice-4ec/v1.1",
 }
